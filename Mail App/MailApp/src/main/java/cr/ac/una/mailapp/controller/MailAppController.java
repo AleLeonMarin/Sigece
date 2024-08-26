a6cf5909356b424322838e503adbee91b8df297a
Kendall Fonseca <kendallfh2004@outlook.com>
2024-08-25 23:40:25 -0600

    [ADD] Vistas para la p[arte principal de la app de Mails

--- Mail App/MailApp/src/main/java/cr/ac/una/mailapp/controller/MailAppController.java
+++ Mail App/MailApp/src/main/java/cr/ac/una/mailapp/controller/MailAppController.java
@@ -4,9 +4,14 @@
  */
 package cr.ac.una.mailapp.controller;
 
+import io.github.palexdev.materialfx.controls.MFXButton;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
 
 /**
  * FXML Controller class
@@ -18,6 +23,28 @@ public class MailAppController extends Controller implements Initializable {
     /**
      * Initializes the controller class.
      */
+    
+      @FXML
+    private MFXButton btnAdminParameters;
+
+    @FXML
+    private MFXButton btnAdminProcess;
+
+    @FXML
+    private MFXButton btnLogOut;
+
+    @FXML
+    private MFXButton btnMailBox;
+
+    @FXML
+    private MFXButton btnMassiveMails;
+
+    @FXML
+    private ImageView imgViewUserPhotProf;
+
+    @FXML
+    private HBox root;
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
@@ -26,5 +53,26 @@ public class MailAppController extends Controller implements Initializable {
     @Override
     public void initialize(){
     }
+    
+    @FXML
+    void onActionBtnAdminParameters(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionBtnAdminProcess(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionBtnMassiveMails(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onBtnMailBox(ActionEvent event) {
+
+    }
+
     
 }
