922b7929d4336227ef7db001bedbb4a239d4f2a5
Kendall Fonseca <kendallfh2004@outlook.com>
2024-08-28 18:44:14 -0600

    Update VariablesMultimediaDto.java

--- Chats And Mail WS/ChatAndMailAPI/src/main/java/cr/ac/una/chatandmailapi/model/VariablesMultimediaDto.java
+++ Chats And Mail WS/ChatAndMailAPI/src/main/java/cr/ac/una/chatandmailapi/model/VariablesMultimediaDto.java
@@ -1,13 +1,74 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package cr.ac.una.chatandmailapi.model;
 
-/**
- *
- * @author Kendall
- */
-public class VariablesMultimediaDto {
-    
+import java.io.Serializable;
+
+public class VariablesMultimediaDto implements Serializable {
+
+    private Long mediaId;
+    private String mediaUrl;
+    private String mediaTipo;
+    private Long mediaVarId;
+    private Long mediaVersion;
+
+    public VariablesMultimediaDto() {
+    }
+
+    public VariablesMultimediaDto(Long mediaId, String mediaUrl, String mediaTipo, Long mediaVarId, Long mediaVersion) {
+        this.mediaId = mediaId;
+        this.mediaUrl = mediaUrl;
+        this.mediaTipo = mediaTipo;
+        this.mediaVarId = mediaVarId;
+        this.mediaVersion = mediaVersion;
+    }
+
+    public Long getMediaId() {
+        return mediaId;
+    }
+
+    public void setMediaId(Long mediaId) {
+        this.mediaId = mediaId;
+    }
+
+    public String getMediaUrl() {
+        return mediaUrl;
+    }
+
+    public void setMediaUrl(String mediaUrl) {
+        this.mediaUrl = mediaUrl;
+    }
+
+    public String getMediaTipo() {
+        return mediaTipo;
+    }
+
+    public void setMediaTipo(String mediaTipo) {
+        this.mediaTipo = mediaTipo;
+    }
+
+    public Long getMediaVarId() {
+        return mediaVarId;
+    }
+
+    public void setMediaVarId(Long mediaVarId) {
+        this.mediaVarId = mediaVarId;
+    }
+
+    public Long getMediaVersion() {
+        return mediaVersion;
+    }
+
+    public void setMediaVersion(Long mediaVersion) {
+        this.mediaVersion = mediaVersion;
+    }
+
+    @Override
+    public String toString() {
+        return "VariablesMultimediaDto{" +
+                "mediaId=" + mediaId +
+                ", mediaUrl='" + mediaUrl + '\'' +
+                ", mediaTipo='" + mediaTipo + '\'' +
+                ", mediaVarId=" + mediaVarId +
+                ", mediaVersion=" + mediaVersion +
+                '}';
+    }
 }
