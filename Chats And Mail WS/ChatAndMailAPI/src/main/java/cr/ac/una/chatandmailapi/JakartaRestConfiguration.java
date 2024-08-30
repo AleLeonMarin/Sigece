608a5fad32c5d0ae6c0e68b247bd24943968e052
Kendall Fonseca <kendallfh2004@outlook.com>
2024-08-29 19:41:36 -0600

    [ADD] DOCUMENTATION AND WS CONFIG

--- Chats And Mail WS/ChatAndMailAPI/src/main/java/cr/ac/una/chatandmailapi/JakartaRestConfiguration.java
+++ Chats And Mail WS/ChatAndMailAPI/src/main/java/cr/ac/una/chatandmailapi/JakartaRestConfiguration.java
@@ -2,6 +2,7 @@ package cr.ac.una.chatandmailapi;
 
 import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.core.Application;
+import cr.ac.una.chatandmailapi.controller.JsonbContextResolver;
 
 /**
  * Configures Jakarta RESTful Web Services for the application.
